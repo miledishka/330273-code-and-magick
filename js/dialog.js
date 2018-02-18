031519356862d96e008867bcaca0e9ecbf11e678
Julija Rodionova <julija.rodionowa@yandex.ru>
2018-02-18 19:09:07 +0300

    Функционал drag&drop

---
+++ js/dialog.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function () {
+  var userPic = document.querySelector('.setup-user-pic');
+
+  var onMouseDownUserPicHandler = function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.setup.style.top = (window.setup.offsetTop - shift.y) + 'px';
+      window.setup.style.left = (window.setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  userPic.addEventListener('mousedown', onMouseDownUserPicHandler);
+})();
